f73c06ae3ccca8f7b9d1e5591a50c842c41b6749
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-18 11:44:19 +0300

    Added graph type

--- src/jgrapht/errors.py
+++ src/jgrapht/errors.py
@@ -23,11 +23,57 @@ class GenericError(Error):
     """
 
     def __init__(self, message):
-        self.message = message        
+        self.message = message
+
+class UnsupportedError(Error):
+    """Exception raised for unsupported errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+class InvalidReferenceError(Error):
+    """Exception raised for invalid reference errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message                        
+
+class IteratorError(Error):
+    """Exception raised for iterator errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
 
 def check_last_error(message=None):
     errno = jgrapht.jgrapht_get_errno()
+    if errno == status.Status.GENERIC_ERROR.value:
+        raise GenericError('An error occured' if message is None else message)
+    if errno == status.Status.UNSUPPORTED_OPERATION.value:
+        raise UnsupportedError('Unsupported Operation' if message is None else message)    
+    if errno == status.Status.INVALID_REFERENCE.value:
+        raise InvalidReferenceError('Invalid reference' if message is None else message)
+    if errno == status.Status.INVALID_GRAPH.value:
+        raise GraphError('Invalid graph' if message is None else message)    
     if errno == status.Status.INVALID_VERTEX.value:
         raise GraphError('Invalid vertex' if message is None else message)
+    if errno == status.Status.INVALID_EDGE.value:
+        raise GraphError('Invalid edge' if message is None else message)
+    if errno == status.Status.GRAPH_CREATION_ERROR.value:
+        raise GraphError('Graph creation error' if message is None else message)
+    if errno == status.Status.GRAPH_IS_UNWEIGHTED.value:
+        raise GraphError('Graph is unweighted' if message is None else message)
+    if errno == status.Status.ITERATOR_NO_SUCH_ELEMENT.value:
+        raise IteratorError('No such element' if message is None else message)                                                            
     if errno != status.Status.SUCCESS.value:
         raise GenericError('Operation failed' if message is None else message)        
--- src/jgrapht/graph.py
+++ src/jgrapht/graph.py
@@ -2,25 +2,122 @@ from . import jgrapht
 from . import status
 from . import errors
 
+class GraphType: 
+
+    def __init__(self, directed, allowing_self_loops, allowing_multiple_edges, weighted):
+        self.__directed = directed
+        self.__allowing_self_loops = allowing_self_loops
+        self.__allowing_multiple_edges = allowing_multiple_edges
+        self.__weighed = weighted
+
+    @property
+    def directed(self):
+        return self.__directed
+
+    @property
+    def undirected(self):
+        return not self.__directed    
+
+    @property
+    def allowing_self_loops(self):
+        return self.__allowing_self_loops
+
+    @property
+    def allowing_multiple_edges(self):
+        return self.__allowing_multiple_edges
+
+    @property
+    def weighted(self):
+        return self.__weighted
+
+    def __str__(self):
+        return '(directed={}, self-loops={}, multiple-edges={}, weighted={})' \
+            .format(self.__directed, self.__allowing_self_loops, self.__allowing_multiple_edges, self.__weighed )
+
+
 class Graph:
 
     def __init__(self):
-        self._g_handle = jgrapht.jgrapht_create_graph(1,1,1)
+        self.__g_handle = jgrapht.jgrapht_create_graph(1,1,1)
+        self.__graph_type = GraphType(True, True, True, True)
         errors.check_last_error()
 
-    def addVertex(self):
-        v = jgrapht.jgrapht_graph_add_vertex(self._g_handle)
+    @property
+    def graph_type(self):
+        return self.__graph_type;
+
+    def add_vertex(self):
+        v = jgrapht.jgrapht_graph_add_vertex(self.__g_handle)
         errors.check_last_error()
         return v
 
-    def removeVertex(self, vertex):
-        res = jgrapht.jgrapht_graph_remove_vertex(self._g_handle, vertex)
+    def remove_vertex(self, vertex):
+        res = jgrapht.jgrapht_graph_remove_vertex(self.__g_handle, vertex)
         errors.check_last_error()
         return res 
 
-    def verticesCount(self):
-        res = jgrapht.jgrapht_graph_vertices_count(self._g_handle)
+    def contains_vertex(self, vertex):
+        res = jgrapht.jgrapht_graph_contains_vertex(self.__g_handle, vertex)
+        errors.check_last_error()
+        return res 
+
+    def vertices_count(self):
+        res = jgrapht.jgrapht_graph_vertices_count(self.__g_handle)
         errors.check_last_error()        
         return res 
 
+    def add_edge(self, source, target):
+        e = jgrapht.jgrapht_graph_add_edge(self.__g_handle, source, target)
+        errors.check_last_error()
+        return e
 
+    def remove_edge(self, edge): 
+        res = jgrapht.jgrapht_graph_remove_edge(self.__g_handle, edge)
+        errors.check_last_error()
+        return res
+
+    def contains_edge(self, edge):
+        res = jgrapht.jgrapht_graph_contains_edge(self.__g_handle, edge)
+        errors.check_last_error()
+        return res     
+
+    def edges_count(self):
+        res = jgrapht.jgrapht_graph_edges_count(self.__g_handle)
+        errors.check_last_error()        
+        return res     
+
+    def degree_of(self, vertex):
+        res = jgrapht.jgrapht_graph_degree_of(self.__g_handle, vertex)
+        errors.check_last_error()        
+        return res     
+
+    def indegree_of(self, vertex):
+        res = jgrapht.jgrapht_graph_indegree_of(self.__g_handle, vertex)
+        errors.check_last_error()        
+        return res
+
+    def outdegree_of(self, vertex):
+        res = jgrapht.jgrapht_graph_outdegree_of(self.__g_handle, vertex)
+        errors.check_last_error()        
+        return res
+
+    def edge_source(self, edge):
+        res = jgrapht.jgrapht_graph_edge_source(self.__g_handle, edge)
+        errors.check_last_error()
+        return res
+
+    def edge_target(self, edge):
+        res = jgrapht.jgrapht_graph_edge_target(self.__g_handle, edge)
+        errors.check_last_error()
+        return res
+
+    def get_edge_weight(self, edge): 
+        res = jgrapht.jgrapht_graph_get_edge_weight(self.__g_handle, edge)
+        errors.check_last_error()
+        return res
+
+    def set_edge_weight(self, edge, weight): 
+        jgrapht.jgrapht_graph_set_edge_weight(self.__g_handle, edge, weight)
+        errors.check_last_error()
+
+    
