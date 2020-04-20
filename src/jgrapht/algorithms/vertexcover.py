a7901c49d9e2e1fe6f7082cdce6ca1069d3dd29f
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-20 12:20:37 +0300

    Added more vertex cover algs

--- src/jgrapht/algorithms/vertexcover.py
+++ src/jgrapht/algorithms/vertexcover.py
@@ -3,7 +3,51 @@ from .. import errors
 from .. import iterator
 
 def vertexcover_greedy(graph):
-    vc_handle = jgrapht.jgrapht_vertexcover_exec_greedy_uniform(graph.handle)
+    vc_handle = jgrapht.jgrapht_vertexcover_exec_greedy(graph.handle)
+    errors.raise_if_last_error()
+    vc_weight = jgrapht.jgrapht_vertexcover_get_weight(vc_handle)
+    errors.raise_if_last_error()
+    vit = iterator.LongValueIterator(jgrapht.jgrapht_vertexcover_create_vit(vc_handle))
+    vc_vertices = list(vit)
+    jgrapht.jgrapht_destroy(vc_handle)
+    errors.raise_if_last_error()
+    return (vc_weight, vc_vertices)
+
+def vertexcover_clarkson(graph):
+    vc_handle = jgrapht.jgrapht_vertexcover_exec_clarkson(graph.handle)
+    errors.raise_if_last_error()
+    vc_weight = jgrapht.jgrapht_vertexcover_get_weight(vc_handle)
+    errors.raise_if_last_error()
+    vit = iterator.LongValueIterator(jgrapht.jgrapht_vertexcover_create_vit(vc_handle))
+    vc_vertices = list(vit)
+    jgrapht.jgrapht_destroy(vc_handle)
+    errors.raise_if_last_error()
+    return (vc_weight, vc_vertices)
+
+def vertexcover_edgebased(graph):
+    vc_handle = jgrapht.jgrapht_vertexcover_exec_edgebased(graph.handle)
+    errors.raise_if_last_error()
+    vc_weight = jgrapht.jgrapht_vertexcover_get_weight(vc_handle)
+    errors.raise_if_last_error()
+    vit = iterator.LongValueIterator(jgrapht.jgrapht_vertexcover_create_vit(vc_handle))
+    vc_vertices = list(vit)
+    jgrapht.jgrapht_destroy(vc_handle)
+    errors.raise_if_last_error()
+    return (vc_weight, vc_vertices)
+
+def vertexcover_baryehudaeven(graph):
+    vc_handle = jgrapht.jgrapht_vertexcover_exec_baryehudaeven(graph.handle)
+    errors.raise_if_last_error()
+    vc_weight = jgrapht.jgrapht_vertexcover_get_weight(vc_handle)
+    errors.raise_if_last_error()
+    vit = iterator.LongValueIterator(jgrapht.jgrapht_vertexcover_create_vit(vc_handle))
+    vc_vertices = list(vit)
+    jgrapht.jgrapht_destroy(vc_handle)
+    errors.raise_if_last_error()
+    return (vc_weight, vc_vertices)
+
+def vertexcover_exact(graph):
+    vc_handle = jgrapht.jgrapht_vertexcover_exec_exact(graph.handle)
     errors.raise_if_last_error()
     vc_weight = jgrapht.jgrapht_vertexcover_get_weight(vc_handle)
     errors.raise_if_last_error()
