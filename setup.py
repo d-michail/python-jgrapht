0fbacc79bfa337b7ecb72db175c074ffa0c7d5a1
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-20 16:07:30 +0300

    First version which actually build the jgrapht-capi

--- setup.py
+++ setup.py
@@ -34,16 +34,34 @@ class BuildConfig(object):
                 pass
 
             def run(self):
-                build_cfg.is_capi_build = build_cfg.compile_capi('vendor/source/jgrapht-capi')
+                build_cfg.is_capi_build = build_cfg.compile_capi()
                 #command = ['echo Hello']
                 #self.announce('Running command: %s' % str(command), level=distutils.log.INFO)
                 #subprocess.check_call(command)
 
         return BuildCapiCommand
 
-    def compile_capi(self, source_folder):
+    def compile_capi(self):
+        """Compile the jgrapht-capi from the git submodule inside `vendor/source/jgrapht-capi`."""
+
+        install_folder = os.path.join("vendor", "install", "jgrapht-capi")
+        install_folder = os.path.abspath(install_folder)
+        if os.path.exists(install_folder):
+            # already compiled and installed, just use it
+            return True
+
+        source_folder = os.path.join("vendor", "source", "jgrapht-capi")
         source_folder = os.path.abspath(source_folder)
-        build_folder = os.path.join(source_folder, 'build')
+        if not os.path.isfile(os.path.join(source_folder, "CMakeLists.txt")):
+            # No git submodule present with vendored source
+            print("Cannot find source in " + source_folder)
+            print("Make sure that you recursively checkout the submodule")
+            print("")
+            return False
+        print("Found source at {}".format(source_folder))
+
+        build_folder = os.path.join("vendor", "build", "jgrapht-capi")
+        build_folder = os.path.abspath(build_folder)
         BuildConfig.create_dir_unless_exists(build_folder)
 
         cwd = os.getcwd()
@@ -51,14 +69,29 @@ class BuildConfig(object):
             os.chdir(build_folder)
 
             print("Configuring jgrapht-capi")
-            retcode = subprocess.call('cmake ..', shell=True)
+            retcode = subprocess.call('cmake {}'.format(source_folder), shell=True)
             if retcode:
                 return False
 
             print("Build jgrapht-capi")
+            print("Using build folder at {}".format(build_folder))
             retcode = subprocess.call('make', shell=True)
             if retcode:
                 return False
+
+            print("Creating jgrapht-capi installation")
+            print("Using install folder at {}".format(install_folder))
+            BuildConfig.create_dir_unless_exists(install_folder)
+            retcode = subprocess.call(
+                'make install', 
+                env=self.create_env(DESTDIR=install_folder),
+                shell=True
+            )
+            if retcode:
+                return False
+
+            return True
+
         finally:
             os.chdir(cwd)
 
@@ -68,6 +101,14 @@ class BuildConfig(object):
         path = os.path.join(dirname)
         if not os.path.isdir(path):
             os.makedirs(path)
+
+    @staticmethod
+    def create_env(**kwargs):
+        env = os.environ.copy()
+        for k, v in kwargs.items():
+            prev = os.environ.get(k)
+            env[k] = "{0} {1}".format(prev, v) if prev else v
+        return env        
 
 class CustomBuild(build):
     def run(self):
