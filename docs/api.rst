817235fc04d6eda5cd91118ff26838e11b563b3e
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-23 19:33:07 +0300

    Added documentation for tests

--- docs/api.rst
+++ docs/api.rst
@@ -14,6 +14,14 @@ As you might expect the main interface is the :class:`Graph <jgrapht.graph.Graph
 
 .. autoclass:: jgrapht.graph.Graph
    :inherited-members:
+   :members:
+
+The following methods allow the user to check for certain graph properties.
+
+.. automodule:: jgrapht.graph
+   :members: is_cubic, is_eulerian
+
+
 
 The :class:`.GraphType` is used to represent during runtime the properties of the graph.
 
--- jgrapht/graph.py
+++ jgrapht/graph.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from copy import copy
 
 from . import jgrapht as backend
-from .errors import raise_status, return_or_raise
+from .errors import raise_status
 from .util import GraphVertexSet, GraphEdgeSet
 from .util import JGraphTLongIterator
 
@@ -382,4 +382,108 @@ def as_edgereversed(graph):
 
 def is_empty_graph(graph):
     err, res = backend.jgrapht_graph_test_is_empty(graph.handle)
+    return res if not err else raise_status()
+
+def is_simple(graph):
+    err, res = backend.jgrapht_graph_test_is_simple(graph.handle)
+    return res if not err else raise_status()
+
+def has_selfloops(graph):
+    err, res = backend.jgrapht_graph_test_has_selfloops(graph.handle)
+    return res if not err else raise_status()
+
+def has_multipleedges(graph):
+    err, res = backend.jgrapht_graph_test_has_multipleedges(graph.handle)
+    return res if not err else raise_status()
+
+def is_complete(graph):
+    err, res = backend.jgrapht_graph_test_is_complete(graph.handle)
+    return res if not err else raise_status()
+
+def is_weakly_connected(graph):
+    err, res = backend.jgrapht_graph_test_is_weakly_connected(graph.handle)
+    return res if not err else raise_status()
+
+def is_strongly_connected(graph):
+    err, res = backend.jgrapht_graph_test_is_strongly_connected(graph.handle)
+    return res if not err else raise_status()
+
+def is_tree(graph):
+    err, res = backend.jgrapht_graph_test_is_tree(graph.handle)
+    return res if not err else raise_status()
+
+def is_forest(graph):
+    err, res = backend.jgrapht_graph_test_is_forest(graph.handle)
+    return res if not err else raise_status()    
+
+def is_overfull(graph):
+    err, res = backend.jgrapht_graph_test_is_overfull(graph.handle)
+    return res if not err else raise_status()    
+
+def is_split(graph):
+    err, res = backend.jgrapht_graph_test_is_split(graph.handle)
+    return res if not err else raise_status()
+
+def is_bipartite(graph):
+    err, res = backend.jgrapht_graph_test_is_bipartite(graph.handle)
+    return res if not err else raise_status()
+
+def is_cubic(graph):
+    """Check whether a graph is `cubic <https://mathworld.wolfram.com/CubicGraph.html>`_.
+
+    A graph is `cubic <https://mathworld.wolfram.com/CubicGraph.html>`_ if all vertices have
+    degree equal to three.
+
+    :param graph: The input graph
+    :returns: True if the graph is cubic, False otherwise.
+    """
+    err, res = backend.jgrapht_graph_test_is_cubic(graph.handle)
+    return res if not err else raise_status()
+
+def is_eulerian(graph):
+    """Check whether a graph is `Eulerian <https://mathworld.wolfram.com/EulerianGraph.html>`_.
+
+    An `Eulerian <https://mathworld.wolfram.com/EulerianGraph.html>`_ graph is a graph containing
+    an `Eulerian cycle <https://mathworld.wolfram.com/EulerianCycle.html>`_.
+
+    :param graph: The input graph
+    :returns: True if the graph is Eulerian, False otherwise.
+    """
+    err, res = backend.jgrapht_graph_test_is_eulerian(graph.handle)
+    return res if not err else raise_status()
+
+def is_chordal(graph):
+    err, res = backend.jgrapht_graph_test_is_chordal(graph.handle)
+    return res if not err else raise_status()
+
+def is_weakly_chordal(graph):
+    err, res = backend.jgrapht_graph_test_is_weakly_chordal(graph.handle)
+    return res if not err else raise_status()
+
+def has_ore(graph):
+    err, res = backend.jgrapht_graph_test_has_ore(graph.handle)
+    return res if not err else raise_status()    
+
+def is_trianglefree(graph):
+    err, res = backend.jgrapht_graph_test_is_trianglefree(graph.handle)
+    return res if not err else raise_status()
+
+def is_perfect(graph):
+    err, res = backend.jgrapht_graph_test_is_perfect(graph.handle)
+    return res if not err else raise_status()
+
+def is_planar(graph):
+    err, res = backend.jgrapht_graph_test_is_planar(graph.handle)
+    return res if not err else raise_status()
+
+def is_kuratowski_subdivision(graph):
+    err, res = backend.jgrapht_graph_test_is_kuratowski_subdivision(graph.handle)
+    return res if not err else raise_status()
+
+def is_k33_subdivision(graph):
+    err, res = backend.jgrapht_graph_test_is_k33_subdivision(graph.handle)
+    return res if not err else raise_status()
+
+def is_k5_subdivision(graph):
+    err, res = backend.jgrapht_graph_test_is_k5_subdivision(graph.handle)
     return res if not err else raise_status()
