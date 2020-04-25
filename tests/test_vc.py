b18a0247ac16baf73fb43218e12d784f75462be4
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-25 17:24:43 +0300

    Improved vc tests

--- tests/test_vc.py
+++ tests/test_vc.py
@@ -3,6 +3,9 @@ import pytest
 import jgrapht.graph as graph
 import jgrapht.algorithms.vertexcover as vc
 
+def assert_same_set(set1, set2):
+    assert set1 <= set2 and set2 <= set1
+
 def build_graph():
     g = graph.Graph(directed=False, allowing_self_loops=True, allowing_multiple_edges=False, weighted=True)
 
@@ -22,29 +25,35 @@ def test_greedy():
     g, vertex_weights = build_graph()
     vc_weight, vc_vertices = vc.vertexcover_greedy(g)
     assert vc_weight == 1.0
-    assert all([a == b for a,b in zip(vc_vertices, [0])]) 
+    assert_same_set(set(vc_vertices), set([0]))
+
 
 def test_greedy_with_weights():
     g, vertex_weights = build_graph()
     vc_weight, vc_vertices = vc.vertexcover_greedy(g, vertex_weights=vertex_weights)
     assert vc_weight == 9.0
-    assert all([a == b for a,b in zip(vc_vertices, [1, 2, 3, 4, 5, 6, 7, 8, 9])]) 
+    assert_same_set(set(vc_vertices), set([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
 
 def test_clarkson():
     g, vertex_weights = build_graph()
     vc_weight, vc_vertices = vc.vertexcover_clarkson(g)
     assert vc_weight == 1.0
-    assert all([a == b for a,b in zip(vc_vertices, [0])]) 
+    assert_same_set(set(vc_vertices), set([0]))
+
 
 def test_clarkson_with_weights():
     g, vertex_weights = build_graph()
     vc_weight, vc_vertices = vc.vertexcover_clarkson(g, vertex_weights=vertex_weights)
     assert vc_weight == 9.0
-    assert all([a == b for a,b in zip(vc_vertices, [1, 2, 3, 4, 5, 6, 7, 8, 9])]) 
+    assert_same_set(set(vc_vertices), set([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
 
+def test_edgebased():
+    g, vertex_weights = build_graph()
+    vc_weight, vc_vertices = vc.vertexcover_edgebased(g)
+    assert vc_weight == 2.0
+    assert_same_set(set(vc_vertices), set([0, 1]))     
 
-#vc_weight, vc_vertices = vc.vertexcover_edgebased(g, vertex_weights=vertex_weights)
-#vc_weight, vc_vertices = vc.vertexcover_edgebased(g)
 
 
