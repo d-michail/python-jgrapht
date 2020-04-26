1e75368a971d37a08a54297699067237b8792433
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-04-26 15:04:38 +0300

    Minor cleanup

--- jgrapht/__init__.py
+++ jgrapht/__init__.py
@@ -25,5 +25,4 @@ del atexit
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
-from logging import NullHandler
-logging.getLogger(__name__).addHandler(NullHandler())
+logging.getLogger(__name__).addHandler(logging.NullHandler())
