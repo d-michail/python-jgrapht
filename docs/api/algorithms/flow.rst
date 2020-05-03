9bc55f05cebdabbb35cbb5f13717886f4a50419e
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-03 12:43:48 +0300

    Improved flow docs

--- docs/api/algorithms/flow.rst
+++ docs/api/algorithms/flow.rst
@@ -1,9 +1,31 @@
 
 .. _algorithms/flow:
 
-Flow
-****
+Flows
+*****
+
+We are given a weighted directed or undirected graph :math:`G(V,E)`. Each edge :math:`e \in E` has 
+an associated non-negative capacity :math:`c_e \ge 0`. The maximum flow problem involves 
+finding a feasible flow :math:`f: E \mapsto \mathbb{R}_{0+}` of maximum value.
+A flow is feasible if:
+
+ * the flow :math:`f(e)` of an edge :math:`e` does not exceed its capacity, and 
+ * for each vertex except the source and sink, the sum of incoming flows is equal to the sum 
+   of outgoing flows.
+
+Computing maximum s-t flows and minimum s-t cuts can be performed using the following 
+functions.
+
+.. autofunction:: jgrapht.algorithms.flow.max_st_flow
+
+.. autofunction:: jgrapht.algorithms.flow.min_st_cut
+
+Algorithms
+----------
+
+When the user requires more advanced control over the selected 
+algorithm, the following functions are provided.
 
 .. automodule:: jgrapht.algorithms.flow
-   :members:
+   :members: push_relabel, dinic, edmonds_karp
 
--- jgrapht/algorithms/flow.py
+++ jgrapht/algorithms/flow.py
@@ -21,18 +21,97 @@ def _maxflow_alg(name, graph, source, sink, *args):
     return flow, cut
 
 def dinic(graph, source, sink):
+    r"""Compute a maximum flow using Dinic's algorithm with scaling.
+    
+    This is a :math:`\mathcal{O}(n^2 m)` algorithm where :math:`n` is the number of vertices 
+    and :math:`m` the number of edges of the graph.
+
+    The algorithm uses the graph edge weights as the network edge capacities.
+    It returns a maximum s-t flow and a minimum s-t cut with the same value.
+    
+    :param graph: The input graph. This can be either directed or undirected. Edge capacities
+                  are taken from the edge weights. 
+    :param source: The source vertex
+    :param sink: The sink vertex.
+    :returns: A tuple (max s-t flow, min s-t cut).
+    """
     return _maxflow_alg('dinic', graph, source, sink)
 
 def push_relabel(graph, source, sink):
+    r"""Compute a maximum flow using the Push-relabel algorithm.
+    
+    This is a :math:`\mathcal{O}(n^3)` algorithm where :math:`n` is the number of vertices 
+    of the graph. For more details on the algorithm see:
+
+      * Andrew V. Goldberg and Robert Tarjan. A new approach to the maximum flow problem.
+        Proceedings of STOC '86.
+
+    The algorithm uses the graph edge weights as the network edge capacities.
+    It returns a maximum s-t flow and a minimum s-t cut with the same value.
+
+    :param graph: The input graph. This can be either directed or undirected. Edge capacities
+                  are taken from the edge weights.
+    :param source: The source vertex
+    :param sink: The sink vertex.
+    :returns: A tuple (max s-t flow, min s-t cut).
+    """
     return _maxflow_alg('push_relabel', graph, source, sink)
 
 def edmonds_karp(graph, source, sink):
+    r"""Compute a maximum flow using the Edmonds-Karp variant of the Ford-Fulkerson algorithm.
+    
+    This is a :math:`\mathcal{O}(n m^2)` algorithm where :math:`n` is the number of vertices 
+    and :math:`m` the number of edges of the graph.
+
+    The algorithm uses the graph edge weights as the network edge capacities.
+    It returns a maximum s-t flow and a minimum s-t cut with the same value.
+
+    .. note:: This implementation assumes that the graph does not contain self-loops or multiple-edges.
+
+    :param graph: The input graph. This can be either directed or undirected. Edge capacities
+                  are taken from the edge weights. 
+    :param source: The source vertex
+    :param sink: The sink vertex.
+    :returns: A tuple (max s-t flow, min s-t cut).
+    """
     return _maxflow_alg('edmonds_karp', graph, source, sink)
 
 def max_st_flow(graph, source, sink):
+    r"""Compute a maximum flow using the Push-relabel algorithm.
+    
+    This is a :math:`\mathcal{O}(n^3)` algorithm where :math:`n` is the number of vertices 
+    of the graph. For more details on the algorithm see:
+
+      * Andrew V. Goldberg and Robert Tarjan. A new approach to the maximum flow problem.
+        Proceedings of STOC '86.
+
+    The algorithm uses the graph edge weights as the network edge capacities.
+
+    :param graph: The input graph. This can be either directed or undirected. Edge capacities
+                  are taken from the edge weights.
+    :param source: The source vertex
+    :param sink: The sink vertex.
+    :returns: The max s-t flow.
+    """
     flow, _ = push_relabel(graph, source, sink)
     return flow
 
 def min_st_cut(graph, source, sink):
+    r"""Compute a minimum s-t cut using the Push-relabel algorithm.
+    
+    This is a :math:`\mathcal{O}(n^3)` algorithm where :math:`n` is the number of vertices 
+    of the graph. For more details on the algorithm see:
+
+      * Andrew V. Goldberg and Robert Tarjan. A new approach to the maximum flow problem.
+        Proceedings of STOC '86.
+
+    The algorithm uses the graph edge weights as the network edge capacities.
+
+    :param graph: The input graph. This can be either directed or undirected. Edge capacities
+                  are taken from the edge weights.
+    :param source: The source vertex
+    :param sink: The sink vertex.
+    :returns: A min s-t cut.
+    """
     _, cut = push_relabel(graph, source, sink)
     return cut
