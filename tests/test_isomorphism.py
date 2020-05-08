7b2cd432280c18fb0e083117437882d840c14217
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-08 22:22:43 +0300

    Added tests for induced subgraph isomorphism

--- tests/test_isomorphism.py
+++ tests/test_isomorphism.py
@@ -51,3 +51,67 @@ def test_iso():
     gm = next(it)
     assert gm.vertices_correspondence() == {0: 8, 1: 7, 2: 6, 3: 5}
     assert gm.edges_correspondence() == {0: 2, 1: 1, 2: 0, 3: 3}
+
+
+def test_iso_induced_subgraph():
+    g1 = create_graph(directed=False, allowing_self_loops=False, allowing_multiple_edges=False, weighted=False)
+
+    g1.add_vertices_from([0, 1, 2, 3])
+
+    g1.add_edge(0, 1)
+    g1.add_edge(1, 2)
+    g1.add_edge(2, 3)
+    g1.add_edge(3, 0)
+
+    g2 = create_graph(directed=False, allowing_self_loops=False, allowing_multiple_edges=False, weighted=False)
+
+    g2.add_vertices_from([5, 6, 7])
+
+    g2.add_edge(5, 6)
+    g2.add_edge(6, 7)
+
+    it = iso.vf2_subgraph(g1, g2)
+
+    assert it is not None
+
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 5, 1: 6, 2: 7, 3: None}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 5, 1: None, 2: 7, 3: 6}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 6, 1: 5, 2: None, 3: 7}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: None, 1: 5, 2: 6, 3: 7}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 7, 1: 6, 2: 5, 3: None}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 7, 1: None, 2: 5, 3: 6}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: 6, 1: 7, 2: None, 3: 5}
+    gm = next(it)
+    assert gm.vertices_correspondence() == {0: None, 1: 7, 2: 6, 3: 5}
+
+
+def test_iso_not_induced_subgraph():
+    g1 = create_graph(directed=False, allowing_self_loops=False, allowing_multiple_edges=False, weighted=False)
+
+    g1.add_vertices_from([0, 1, 2, 3])
+
+    g1.add_edge(0, 1)
+    g1.add_edge(1, 2)
+    g1.add_edge(2, 3)
+    g1.add_edge(3, 0)
+    g1.add_edge(0, 2)
+    g1.add_edge(1, 3)
+
+    g2 = create_graph(directed=False, allowing_self_loops=False, allowing_multiple_edges=False, weighted=False)
+
+    g2.add_vertices_from([5, 6, 7])
+
+    g2.add_edge(5, 6)
+    g2.add_edge(6, 7)
+
+    it = iso.vf2_subgraph(g1, g2)
+
+    assert it is None
+
