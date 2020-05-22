85e4f4013d6f5425517d8748eacdfda76efcd874
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-05-23 00:36:01 +0300

    Added gexf support for edge lists

--- jgrapht/io/edgelist.py
+++ jgrapht/io/edgelist.py
@@ -43,9 +43,8 @@ def read_edgelist_json(
             ]
         }
 
-    In case the graph is weighted then the importer also reads edge weights. Otherwise edge weights
-    are ignored. The importer also supports reading additional string attributes such as label or
-    custom user attributes. The parser completely ignores elements from the input that are not related
+    The importer also supports reading additional string attributes such as label or custom user
+    attributes. The parser completely ignores elements from the input that are not related
     to vertices or edges of the graph. Moreover, complicated nested structures which are inside
     vertices or edges are simply returned as a whole. For example, in the following graph::
 
@@ -113,9 +112,8 @@ def parse_edgelist_json(
             ]
         }
 
-    In case the graph is weighted then the importer also reads edge weights. Otherwise edge weights
-    are ignored. The importer also supports reading additional string attributes such as label or
-    custom user attributes. The parser completely ignores elements from the input that are not related
+    The importer also supports reading additional string attributes such as label or custom user
+    attributes. The parser completely ignores elements from the input that are not related
     to vertices or edges of the graph. Moreover, complicated nested structures which are inside
     vertices or edges are simply returned as a whole. For example, in the following graph::
 
@@ -159,3 +157,164 @@ def parse_edgelist_json(
         args = [import_id_f_ptr, vertex_attribute_f_ptr, edge_attribute_f_ptr]
 
     return _import_edgelist('string_json', with_attrs, input_string, *args)
+
+
+def read_edgelist_gexf(
+    filename,
+    import_id_cb,
+    validate_schema=True,
+    vertex_attribute_cb=None,
+    edge_attribute_cb=None,
+):
+    """Imports a graph as an edgelist from a GEXF file.
+
+    This is a simple implementation with supports only a limited set of features of the GEXF specification,
+    oriented towards parsing speed. Moreover, it notifies lazily and completely out-of-order for any additional
+    vertex and edge attributes in the input file. Users can register callbacks for vertex and edge attributes.
+    Finally, default attribute values and any nested elements are completely ignored.
+
+    For a description of the format see https://gephi.org/gexf/format/index.html or the 
+    `GEXF Primer <https://gephi.org/gexf/format/primer.html>`_.
+
+    Below is small example of a graph in GEXF format::
+
+        <?xml version="1.0" encoding="UTF-8"?>
+        <gexf xmlns="http://www.gexf.net/1.2draft"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd"
+            version="1.2">
+            <graph defaultedgetype="undirected">
+                <nodes>
+                <node id="n0" label="node 0"/>
+                <node id="n1" label="node 1"/>
+                <node id="n2" label="node 2"/>
+                <node id="n3" label="node 3"/>
+                <node id="n4" label="node 4"/>
+                <node id="n5" label="node 5"/>
+                </nodes>
+                <edges>
+                <edge id="e0" source="n0" target="n2" weight="1.0"/>
+                <edge id="e1" source="n0" target="n1" weight="1.0"/>
+                <edge id="e2" source="n1" target="n3" weight="2.0"/>
+                <edge id="e3" source="n3" target="n2"/>
+                <edge id="e4" source="n2" target="n4"/>
+                <edge id="e5" source="n3" target="n5"/>
+                <edge id="e6" source="n5" target="n4" weight="1.1"/>
+                </edges>
+            </graph>
+        </gexf>
+
+    The parser completely ignores the global attribute "defaultedgetype" and the edge attribute "type"
+    which denotes whether an edge is directed or not. The importer by default validates the input
+    using the 1.2draft GEXF Schema. The user can (not recommended) disable the validation by adjusting
+    the appropriate parameter. Older schemas are not supported.
+
+    .. note:: The import identifier callback accepts a single parameter which is the identifier read
+              from the input file as a string. It should return a integer with the identifier of the 
+              graph vertex.
+
+    .. note:: Attribute callback functions accept three parameters. The first is the vertex
+              or edge identifier. The second is the attribute key and the third is the 
+              attribute value.
+
+    :param graph: the graph to read into
+    :param filename: the input file to read from
+    :param import_id_cb: callback to transform identifiers from file to integer vertices.
+    :param validate_schema: whether to validate the XML schema    
+    :param vertex_attribute_cb: callback function for vertex attributes
+    :param edge_attribute_cb: callback function for edge attributes
+    :returns: an edge list. This is an iterator which returns named tuples(source, target, weight)        
+    :raises IOError: in case of an import error    
+    """
+    import_id_f_ptr, _ = _create_wrapped_import_id_callback(import_id_cb)
+
+    if vertex_attribute_cb is None and edge_attribute_cb is None: 
+        with_attrs = False
+        args = [import_id_f_ptr, validate_schema]
+    else:
+        with_attrs = True
+        vertex_attribute_f_ptr, _ = _create_wrapped_attribute_callback(vertex_attribute_cb)
+        edge_attribute_f_ptr, _ = _create_wrapped_attribute_callback(edge_attribute_cb)
+        args = [import_id_f_ptr, validate_schema, vertex_attribute_f_ptr, edge_attribute_f_ptr]
+
+    return _import_edgelist('file_gexf', with_attrs, filename, *args)
+
+
+def parse_edgelist_gexf(
+    input_string,
+    import_id_cb,
+    validate_schema=True,
+    vertex_attribute_cb=None,
+    edge_attribute_cb=None,
+):
+    """Imports a graph as an edgelist from a GEXF input string.
+
+    This is a simple implementation with supports only a limited set of features of the GEXF specification,
+    oriented towards parsing speed. Moreover, it notifies lazily and completely out-of-order for any
+    additional vertex and edge attributes in the input file. Users can register callbacks for vertex and
+    edge attributes. Finally, default attribute values and any nested elements are completely ignored.
+
+    For a description of the format see https://gephi.org/gexf/format/index.html or the 
+    `GEXF Primer <https://gephi.org/gexf/format/primer.html>`_.
+
+    Below is small example of a graph in GEXF format::
+
+        <?xml version="1.0" encoding="UTF-8"?>
+        <gexf xmlns="http://www.gexf.net/1.2draft"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd"
+            version="1.2">
+            <graph defaultedgetype="undirected">
+                <nodes>
+                <node id="n0" label="node 0"/>
+                <node id="n1" label="node 1"/>
+                <node id="n2" label="node 2"/>
+                <node id="n3" label="node 3"/>
+                <node id="n4" label="node 4"/>
+                <node id="n5" label="node 5"/>
+                </nodes>
+                <edges>
+                <edge id="e0" source="n0" target="n2" weight="1.0"/>
+                <edge id="e1" source="n0" target="n1" weight="1.0"/>
+                <edge id="e2" source="n1" target="n3" weight="2.0"/>
+                <edge id="e3" source="n3" target="n2"/>
+                <edge id="e4" source="n2" target="n4"/>
+                <edge id="e5" source="n3" target="n5"/>
+                <edge id="e6" source="n5" target="n4" weight="1.1"/>
+                </edges>
+            </graph>
+        </gexf>
+
+    The parser completely ignores the global attribute "defaultedgetype" and the edge attribute "type" which
+    denotes whether an edge is directed or not. The importer by default validates the input using the 1.2draft
+    GEXF Schema. The user can (not recommended) disable the validation by adjusting the appropriate parameter.
+    Older schemas are not supported.
+
+    .. note:: The import identifier callback accepts a single parameter which is the identifier read
+              from the input file as a string. It should return a integer with the identifier of the 
+              graph vertex.
+
+    .. note:: Attribute callback functions accept three parameters. The first is the vertex
+              or edge identifier. The second is the attribute key and the third is the 
+              attribute value.
+
+    :param input_string: the input string to read from
+    :param import_id_cb: callback to transform identifiers from file to integer vertices.
+    :param validate_schema: whether to validate the XML schema    
+    :param vertex_attribute_cb: callback function for vertex attributes
+    :param edge_attribute_cb: callback function for edge attributes
+    :returns: an edge list. This is an iterator which returns named tuples(source, target, weight)        
+    :raises IOError: in case of an import error    
+    """
+    import_id_f_ptr, _ = _create_wrapped_import_id_callback(import_id_cb)
+
+    if vertex_attribute_cb is None and edge_attribute_cb is None: 
+        with_attrs = False
+        args = [import_id_f_ptr, validate_schema]
+    else:
+        with_attrs = True
+        vertex_attribute_f_ptr, _ = _create_wrapped_attribute_callback(vertex_attribute_cb)
+        edge_attribute_f_ptr, _ = _create_wrapped_attribute_callback(edge_attribute_cb)
+        args = [import_id_f_ptr, validate_schema, vertex_attribute_f_ptr, edge_attribute_f_ptr]
+
+    return _import_edgelist('string_gexf', with_attrs, input_string, *args)
---
+++ tests/test_edgelist_gexf.py
@@ -0,0 +1,148 @@
+import pytest
+
+from jgrapht import create_graph
+from jgrapht.io.edgelist import read_edgelist_gexf, parse_edgelist_gexf
+
+
+input1 = r"""<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns="http://www.gexf.net/1.2draft"
+    version="1.2" 
+    xsi:schemaLocation="http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <graph defaultedgetype="undirected">
+        <nodes>
+            <node id="1" label="κόμβος 1"/>
+            <node id="2" label="mylabel 2"/>
+            <node id="3" label="3"/>
+        </nodes>
+        <edges>
+            <edge id="1" source="2" target="3" />
+            <edge id="0" source="1" target="2" />
+            <edge id="2" source="3" target="1" />
+        </edges>
+        </graph>
+</gexf>
+"""
+
+expected = r"""<?xml version="1.0" encoding="UTF-8"?><gexf xmlns="http://www.gexf.net/1.2draft" xsi:schemaLocation="http://www.gexf.net/1.2draft http://www.gexf.net/1.2draft/gexf.xsd" version="1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <graph defaultedgetype="directed">
+        <nodes>
+            <node id="0" label="0"/>
+            <node id="1" label="1"/>
+            <node id="2" label="2"/>
+            <node id="3" label="3"/>
+        </nodes>
+        <edges>
+            <edge id="0" source="0" target="1"/>
+            <edge id="1" source="0" target="2"/>
+            <edge id="2" source="0" target="3"/>
+            <edge id="3" source="2" target="3"/>
+        </edges>
+    </graph>
+</gexf>
+"""
+
+def test_input_gexf(tmpdir):
+    tmpfile = tmpdir.join("gexf.out")
+    tmpfilename = str(tmpfile)
+
+    # write file json with escaped characters
+    with open(tmpfilename, "w") as f:
+        f.write(input1)
+
+    v_attrs = dict()
+    e_attrs = dict()
+
+    # test that you read back unescaped
+    def va_cb(vertex, attribute_name, attribute_value):
+        if vertex not in v_attrs: 
+            v_attrs[vertex] = {}
+        v_attrs[vertex][attribute_name] = attribute_value    
+
+    def ea_cb(edge, attribute_name, attribute_value):
+        if edge not in e_attrs: 
+            e_attrs[edge] = {}
+        e_attrs[edge][attribute_name] = attribute_value
+
+    def import_id_cb(id):
+        return int(id)
+
+    edgelist = read_edgelist_gexf(
+        tmpfilename,
+        import_id_cb=import_id_cb,
+        vertex_attribute_cb=va_cb,
+        edge_attribute_cb=ea_cb,
+    )
+
+    assert v_attrs[1]["label"] == "κόμβος 1"
+    assert v_attrs[2]["label"] == "mylabel 2"
+
+    assert e_attrs == {0: {'id': '1', 'source': '2', 'target': '3'}, 1: {'id': '0', 'source': '1', 'target': '2'}, 2: {'id': '2', 'source': '3', 'target': '1'}}
+
+    assert list(edgelist) == [(2,3,1), (1,2,1), (3,1,1)]
+
+
+
+def test_input_gexf_no_attrs(tmpdir):
+    tmpfile = tmpdir.join("gexf.out")
+    tmpfilename = str(tmpfile)
+
+    # write file json with escaped characters
+    with open(tmpfilename, "w") as f:
+        f.write(input1)
+
+    def import_id_cb(id):
+        return int(id)
+
+    edgelist = read_edgelist_gexf(
+        tmpfilename,
+        import_id_cb=import_id_cb,
+    )
+
+    assert list(edgelist) == [(2,3,1), (1,2,1), (3,1,1)]
+
+
+def test_input_gexf_from_string_no_attrs(tmpdir):
+    def import_id_cb(id):
+        return int(id)
+
+    edgelist = parse_edgelist_gexf(
+        input1,
+        import_id_cb=import_id_cb,
+    )
+
+    assert list(edgelist) == [(2,3,1), (1,2,1), (3,1,1)]
+
+
+def test_input_gexf_from_string(tmpdir):
+    v_attrs = dict()
+    e_attrs = dict()
+
+    # test that you read back unescaped
+    def va_cb(vertex, attribute_name, attribute_value):
+        if vertex not in v_attrs: 
+            v_attrs[vertex] = {}
+        v_attrs[vertex][attribute_name] = attribute_value    
+
+    def ea_cb(edge, attribute_name, attribute_value):
+        if edge not in e_attrs: 
+            e_attrs[edge] = {}
+        e_attrs[edge][attribute_name] = attribute_value
+
+    def import_id_cb(id):
+        return int(id)
+
+    edgelist = parse_edgelist_gexf(
+        input1,
+        import_id_cb=import_id_cb,
+        vertex_attribute_cb=va_cb,
+        edge_attribute_cb=ea_cb,
+    )
+
+    assert v_attrs[1]["label"] == "κόμβος 1"
+    assert v_attrs[2]["label"] == "mylabel 2"
+
+    assert e_attrs == {0: {'id': '1', 'source': '2', 'target': '3'}, 1: {'id': '0', 'source': '1', 'target': '2'}, 2: {'id': '2', 'source': '3', 'target': '1'}}
+
+    assert list(edgelist) == [(2,3,1), (1,2,1), (3,1,1)]
+    
