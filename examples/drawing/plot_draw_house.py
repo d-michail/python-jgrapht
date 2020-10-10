4b35579173378932fe4596154c36156d64ee4949
Chris Voliotis <45997773+Chris-Voliotis@users.noreply.github.com>
2020-10-10 18:19:16 +0300

    Update plot_draw_house.py

--- examples/drawing/plot_draw_house.py
+++ examples/drawing/plot_draw_house.py
@@ -9,8 +9,8 @@ Draw a graph with matplotlib. You must have matplotlib for this to work.
 
 # %%
 # Start by importing the package
-
-import jgrapht.drawing.draw_matplotlibimport draw
+import jgrapht
+import jgrapht.drawing.draw_matplotlib as draw_matplotlib
 import matplotlib.pyplot as plt
 
 
@@ -43,10 +43,10 @@ e4 = g.add_edge(3, 4)
 # %%
 # Draw the nodes
 pos = [(0, 0), (1, 0), (0, 1), (1, 1), (0.5, 2.0)]
-draw.draw_nodes(g, position=pos, node_list=(0, 1, 2, 3), node_color="green")
-draw.draw_nodes(g, position=pos, node_list=[4], node_color="red", node_shape="^")
+draw_matplotlib.draw_jgrapht_vertices(g, position=pos, node_list=(0, 1, 2, 3), node_color="green")
+draw_matplotlib.draw_jgrapht_vertices(g, position=pos, node_list=[4], node_color="red", node_shape="^")
 
 # Draw the edges
-draw.draw_edges(g, position=pos, edge_color="orange")
+draw_matplotlib.draw_jgrapht_edges(g, position=pos, edge_color="orange")
 plt.tight_layout()
 plt.show()
