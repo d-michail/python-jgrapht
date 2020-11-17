f5d3603ef2073e1136d4571a8637dc2f89a3d191
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-11-17 13:15:06 +0200

    Added methods for converting from/to nx

--- jgrapht/convert.py
+++ jgrapht/convert.py
@@ -7,8 +7,10 @@ def _strip(elem):
         return elem.strip('"')
     return elem
 
+
 def _get_name(elem):
     return _strip(elem.get_name())
+
 
 def _to_vertex_list(pydot_vertex):
     result = []
@@ -18,6 +20,7 @@ def _to_vertex_list(pydot_vertex):
         for u in pydot_vertex["nodes"]:
             result.append(_strip(u))
     return result
+
 
 def from_pydot(graph):
     """Create a graph from a pydot graph.
@@ -144,3 +147,141 @@ def to_pydot(graph):
         dotg.add_edge(dote)
 
     return dotg
+
+
+def from_nx(graph, any_hashable=True):
+    """Create a graph from a NetworkX graph.
+
+    :param graph: the graph in nx format
+    :type graph: nx graph
+    :param any_hashable: if true the returned graph uses the same objects as the nx graph,
+        otherwise integers are used. In the later case a renumbering is performed independently from 
+        any possible ordering in the original graph.
+    :type any_hashable: boolean
+    :returns: the graph
+    """
+    try:
+        import networkx as nx
+    except ImportError:
+        raise ImportError("NetworkX required")
+
+    is_directed = nx.is_directed(graph)
+    is_weighted = any("weight" in data for u, v, data in graph.edges(data=True))
+    allowing_multiple_edges = isinstance(graph, (nx.MultiGraph, nx.MultiDiGraph))
+
+    result = _create_graph(
+        directed=is_directed,
+        weighted=is_weighted,
+        allowing_self_loops=True,
+        allowing_multiple_edges=allowing_multiple_edges,
+        any_hashable=any_hashable,
+    )
+
+    if any_hashable:
+        # copy graph topology and attributes
+        result.graph_attrs.update(**graph.graph)
+
+        for v in graph.nodes:
+            result.add_vertex(vertex=v)
+            result.vertex_attrs[v].update(**graph.nodes[v])
+
+        try: 
+            for u, v, k in graph.edges(keys=True):
+                result.add_edge(u, v, edge=k)
+                result.edge_attrs[k].update(**graph.edges[u,v,k])
+        except TypeError:
+            for u, v in graph.edges():
+                e = result.add_edge(u, v)
+                result.edge_attrs[e].update(**graph.edges[u,v])
+    else: 
+        # copy graph topology only
+        for v in graph.nodes:
+            result.add_vertex(vertex=v)
+
+        for u, v, d in graph.edges(data=True):
+            if 'weight' in d: 
+                result.add_edge(u, v, weight=d['weight'])    
+            else: 
+                result.add_edge(u, v)
+
+    return result
+
+
+def to_nx(graph):
+    """Create a NetworkX graph from a graph.
+
+    :param graph: the graph
+    :type graph: graph
+    :returns: a graph
+    :rtype: NetworkX graph
+    """
+    try:
+        import networkx as nx
+    except ImportError:
+        raise ImportError("NetworkX required")
+
+    if graph.type.directed: 
+        if graph.type.allowing_multiple_edges:
+            result = nx.MultiDiGraph()
+        else:
+            result = nx.DiGraph()
+    else:
+        if graph.type.allowing_multiple_edges:
+            result = nx.MultiGraph()
+        else:
+            result = nx.Graph()
+
+    if _is_anyhashable_graph(graph):
+        # copy topology and attributes
+        result.graph.update(**graph.graph_attrs)
+
+        for v in graph.vertices: 
+            result.add_node(v)
+            result.nodes[v].update(**graph.vertex_attrs[v])
+
+        if graph.type.allowing_multiple_edges: 
+            if graph.type.weighted:
+                for e in graph.edges:
+                    u = graph.edge_source(e)
+                    v = graph.edge_target(e)
+                    w = graph.get_edge_weight(e)
+                    result.add_edge(u, v, key=e, weight=w)
+                    result.edges[u, v, e].update(**graph.edge_attrs[e])
+            else:
+                for e in graph.edges:
+                    u = graph.edge_source(e)
+                    v = graph.edge_target(e)
+                    result.add_edge(u, v, key=e)
+                    result.edges[u, v, e].update(**graph.edge_attrs[e])
+        else:
+            if graph.type.weighted:
+                for e in graph.edges:
+                    u = graph.edge_source(e)
+                    v = graph.edge_target(e)
+                    w = graph.get_edge_weight(e)
+                    result.add_edge(u, v, weight=w)
+                    result.edges[u, v].update(**graph.edge_attrs[e])
+            else:
+                for e in graph.edges:
+                    u = graph.edge_source(e)
+                    v = graph.edge_target(e)
+                    result.add_edge(u, v)
+                    result.edges[u, v].update(**graph.edge_attrs[e])
+    else:
+        # copy topology
+        for v in graph.vertices: 
+            result.add_node(v)
+
+        if graph.type.weighted:
+            for e in graph.edges:
+                u = graph.edge_source(e)
+                v = graph.edge_target(e)
+                w = graph.get_edge_weight(e)
+                result.add_edge(u, v, weight=w)
+        else:
+            for e in graph.edges:
+                u = graph.edge_source(e)
+                v = graph.edge_target(e)
+                result.add_edge(u, v)
+
+    return result
