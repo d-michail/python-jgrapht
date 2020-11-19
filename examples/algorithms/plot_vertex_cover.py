41b084af8d810dcba511aef931f2eb4e971caeac
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-11-19 17:10:33 +0200

    Fixed typos in example

--- examples/algorithms/plot_vertex_cover.py
+++ examples/algorithms/plot_vertex_cover.py
@@ -4,8 +4,8 @@
 Vertex Cover
 ============
 
-In this example we demostrate how to find compute a vertex cover in
-undirected graphs.
+In this example we demostrate how to compute a vertex cover in an
+undirected graph.
 """
 
 # %%
@@ -17,7 +17,7 @@ import jgrapht.drawing.draw_matplotlib as drawing
 import matplotlib.pyplot as plt
 
 # %%
-# We start by creating a "star" graph with a few additional edges.
+# We start by creating a "star" graph.
 
 g = jgrapht.create_graph(directed=False, weighted=False)
 
@@ -30,12 +30,12 @@ for i in range(10):
 print(g)
 
 # %%
-# Then, we execute the 2 approximation algorithm of R. Bar-Yehuda and S. Even.
+# Then, we execute the 2-approximation algorithm of R. Bar-Yehuda and S. Even.
 
 weight, vertex_cover = vc.baryehuda_even(g)
 
 # %%
-# The result is a tuple which contains whether the weight and the vertex cover.
+# The result is a tuple which contains the weight and the vertex cover.
 # Although it failed to find the optimum which is 1.0, the returned solution is 
 # at most twice the optimum.
 # 
@@ -44,8 +44,7 @@ print('Vertex cover: {}'.format(vertex_cover))
 
 
 # %%
-# Ploting the graph and separate color for the vertex cover vertices.
-# component.
+# Ploting the graph using separate color for the vertices in the vertex cover.
 positions = drawing.layout(g, name="fruchterman_reingold", seed=17)
 
 drawing.draw_jgrapht_vertices(g, positions=positions, vertex_list=vertex_cover, vertex_color='red')
