a4b364ef7fb023ca9335623e5f2fab543d8f03f3
Dimitrios Michail <dimitrios.michail@gmail.com>
2020-12-29 10:18:50 +0200

    Test generators and contraction hierarchies

--- tests/test_contraction_hierarchies.py
+++ tests/test_contraction_hierarchies.py
@@ -11,35 +11,35 @@ def get_graph(backend):
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
 
     for i in range(0, 8):
         g.add_vertex(i)
 
-    g.add_edge(0, 1, weight=3.0)
-    g.add_edge(1, 3, weight=100.0)
-    g.add_edge(0, 2, weight=40.0)
-    g.add_edge(2, 4, weight=20.0)
-    g.add_edge(3, 5, weight=2.0)
-    g.add_edge(4, 5, weight=2.0)
-    g.add_edge(5, 0, weight=13.0)
-    g.add_edge(0, 5, weight=1000.0)
-    g.add_edge(5, 6, weight=12.0)
-    g.add_edge(5, 7, weight=20.0)
-    g.add_edge(6, 7, weight=3.0)
-    g.add_edge(4, 7, weight=200.0)
+    g.add_edge(0, 1, edge=0, weight=3.0)
+    g.add_edge(1, 3, edge=1, weight=100.0)
+    g.add_edge(0, 2, edge=2, weight=40.0)
+    g.add_edge(2, 4, edge=3, weight=20.0)
+    g.add_edge(3, 5, edge=4, weight=2.0)
+    g.add_edge(4, 5, edge=5, weight=2.0)
+    g.add_edge(5, 0, edge=6, weight=13.0)
+    g.add_edge(0, 5, edge=7, weight=1000.0)
+    g.add_edge(5, 6, edge=8, weight=12.0)
+    g.add_edge(5, 7, edge=9, weight=20.0)
+    g.add_edge(6, 7, edge=10, weight=3.0)
+    g.add_edge(4, 7, edge=11, weight=200.0)
 
     return g
 
 
-def get_anyhashableg_graph():
+def get_anyhashableg_graph(backend):
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        any_hashable=True,
+        backend=backend,
     )
 
     for i in range(0, 8):
@@ -61,8 +61,16 @@ def get_anyhashableg_graph():
     return g
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
-def test_ch_dijkstra(backend): 
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
+def test_ch_dijkstra(backend):
     g = get_graph(backend)
 
     ch = sp.precompute_contraction_hierarchies(g, parallelism=1, seed=31)
@@ -82,8 +90,11 @@ def test_ch_dijkstra(backend):
     assert 6 == p2.end_vertex
 
 
-def test_ch_dijkstra_anyhashable(): 
-    g = get_anyhashableg_graph()
+@pytest.mark.parametrize(
+    "backend", [GraphBackend.ANY_HASHABLE_GRAPH, GraphBackend.REFCOUNT_GRAPH]
+)
+def test_ch_dijkstra_anyhashable(backend):
+    g = get_anyhashableg_graph(backend)
 
     ch = sp.precompute_contraction_hierarchies(g, parallelism=1, seed=31)
 
@@ -102,13 +113,21 @@ def test_ch_dijkstra_anyhashable():
     assert 6 == p2.end_vertex
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
-def test_ch_many_to_many(backend): 
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
+def test_ch_many_to_many(backend):
     g = get_graph(backend)
 
     ch = sp.precompute_contraction_hierarchies(g, parallelism=1, seed=31)
 
-    mm = sp.contraction_hierarchies_many_to_many(g, {0,1}, {6,7}, ch=ch)
+    mm = sp.contraction_hierarchies_many_to_many(g, {0, 1}, {6, 7}, ch=ch)
 
     p1 = mm.get_path(0, 7)
 
@@ -128,17 +147,24 @@ def test_ch_many_to_many(backend):
         mm.get_path(2, 6)
 
 
-def test_anyhashable_ch_many_to_many(): 
-    g = get_anyhashableg_graph()
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
+def test_anyhashable_ch_many_to_many(backend):
+    g = get_anyhashableg_graph(backend)
 
     ch = sp.precompute_contraction_hierarchies(g, parallelism=1, seed=31)
 
-    mm = sp.contraction_hierarchies_many_to_many(g, {0,1}, {6,7}, ch=ch)
+    mm = sp.contraction_hierarchies_many_to_many(g, {0, 1}, {6, 7}, ch=ch)
 
     p1 = mm.get_path(0, 7)
 
     assert 77 == p1.weight
-    assert ['2', 3, 5, 8, 10] == p1.edges
+    assert ["2", 3, 5, 8, 10] == p1.edges
     assert 0 == p1.start_vertex
     assert 7 == p1.end_vertex
 
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,77 +2,102 @@ import pytest
 
 from jgrapht import create_graph, GraphBackend
 import jgrapht.generators as generators
+from jgrapht.utils import create_vertex_supplier, create_edge_supplier
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_barabasi_albert(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.barabasi_albert(g, 10, 5, 100)
     assert len(g.vertices) == 100
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_barabasi_albert_forest(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.barabasi_albert_forest(g, 10, 100)
     assert len(g.vertices) == 100
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_complete(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.complete_graph(g, 10)
     assert len(g.vertices) == 10
 
-def test_complete_property_graph():
 
-    # Test that changes performed by the generators in the 
-    # backend graph are propagated to the property graph.
-
-    g = create_graph(
-        directed=False,
-        allowing_self_loops=False,
-        allowing_multiple_edges=False,
-        weighted=True,
-        any_hashable=True,
-    )
-
-    generators.complete_graph(g, 10)
-    assert len(g.vertices) == 10
-
-
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_complete_bipartite(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.complete_bipartite_graph(g, 10, 10)
     assert len(g.vertices) == 20
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_empty(backend):
     g = create_graph(
         directed=False,
@@ -85,85 +110,141 @@ def test_empty(backend):
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_gnm_random(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.gnm_random_graph(g, 10, 30)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_gnp_random(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.gnp_random_graph(g, 10, 0.2)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_ring(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.ring_graph(g, 10)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_scalefree(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.scalefree_graph(g, 10)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_watts_strogatz(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.watts_strogatz_graph(g, 10, 2, 0.1)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_kleinberg_smallworld(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.kleinberg_smallworld_graph(g, 10, 2, 2, 1)
     assert len(g.vertices) == 100
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_complement(backend):
     g_source = create_graph(
         directed=False,
@@ -175,15 +256,17 @@ def test_complement(backend):
     g_source.add_vertex(0)
     g_source.add_vertex(1)
     g_source.add_vertex(2)
-    g_source.add_edge(0, 1)
-    g_source.add_edge(0, 2)
+    g_source.add_edge(0, 1, edge=0)
+    g_source.add_edge(0, 2, edge=1)
 
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        vertex_supplier=create_vertex_supplier(type="int"),
+        edge_supplier=create_edge_supplier(type="int"),
+        backend=backend,
     )
 
     generators.complement_graph(g, g_source)
@@ -193,131 +276,211 @@ def test_complement(backend):
     assert g.edge_tuple(0) == (1, 2, 1.0)
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_generalized_petersen(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.generalized_petersen(g, 10, 4)
     assert len(g.vertices) == 20
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_grid(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.grid(g, 10, 4)
     assert len(g.vertices) == 40
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_hypercube(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.hypercube(g, 4)
     assert len(g.vertices) == 16
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_linear(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.linear(g, 10)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_random_regular(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.random_regular(g, 10, 3, 17)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_random_regular_no_seed(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.random_regular(g, 10, 3)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_star(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.star(g, 10)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_wheel(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.wheel(g, 10, False)
     assert len(g.vertices) == 10
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_windmill(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.windmill(g, 3, 3, dutch=False)
     assert len(g.vertices) == 7
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_linearized_chord_diagram(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=True,
         allowing_multiple_edges=True,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.linearized_chord_diagram(g, 100, 3, 17)
     assert len(g.vertices) == 100
@@ -328,20 +491,28 @@ def test_linearized_chord_diagram(backend):
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     with pytest.raises(ValueError):
         generators.linearized_chord_diagram(g, 100, 3, 17)
 
 
-@pytest.mark.parametrize("backend", [GraphBackend.INT_GRAPH, GraphBackend.LONG_GRAPH])
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REFCOUNT_GRAPH,
+    ],
+)
 def test_linearized_chord_diagram_no_seed(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=True,
         allowing_multiple_edges=True,
         weighted=True,
-        backend=backend
+        backend=backend,
     )
     generators.linearized_chord_diagram(g, 100, 3)
     assert len(g.vertices) == 100
