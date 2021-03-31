9b5a306276a6dc5d518bc14a410dceed3ed72209
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-03-31 16:07:20 +0300

    Added tests for connectivity

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -1,15 +1,28 @@
 import pytest
 
-from jgrapht import create_graph
+from jgrapht import create_graph, GraphBackend
+from jgrapht.utils import IntegerSupplier
 import jgrapht.algorithms.connectivity as connectivity
 
 
-def test_weakly():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+    ],
+)
+def test_weakly(backend):
     g = create_graph(
         directed=False,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),        
     )
 
     g.add_vertices_from([0, 1, 2, 3])
@@ -40,12 +53,24 @@ def test_weakly():
     assert component2 == set([4, 5])
 
 
-def test_weakly_directed():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+    ],
+)
+def test_weakly_directed(backend):
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),                
     )
 
     g.add_vertices_from([0, 1, 2, 3])
@@ -76,12 +101,24 @@ def test_weakly_directed():
     assert component2 == set([4, 5])
 
 
-def test_strongly_kosaraju():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+    ],
+)
+def test_strongly_kosaraju(backend):
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),                
     )
 
     g.add_vertices_from([0, 1, 2, 3, 4, 5])
@@ -116,12 +153,24 @@ def test_strongly_kosaraju():
     assert component1 == set([0, 1, 2, 3, 4, 5])
 
 
-def test_strongly_gabow():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+    ],
+)
+def test_strongly_gabow(backend):
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),                
     )
 
     g.add_vertices_from([0, 1, 2, 3, 4, 5])
@@ -156,13 +205,25 @@ def test_strongly_gabow():
     assert component1 == set([0, 1, 2, 3, 4, 5])
 
 
-def test_is_connected():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+        GraphBackend.INT_GRAPH,
+        GraphBackend.LONG_GRAPH,
+    ],
+)
+def test_is_connected(backend):
     # directed
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),
     )
 
     g.add_vertices_from([0, 1])
@@ -188,13 +249,22 @@ def test_is_connected():
     assert is_connected
 
 
-def test_anyhashableg_strongly_gabow():
+@pytest.mark.parametrize(
+    "backend",
+    [
+        GraphBackend.ANY_HASHABLE_GRAPH,
+        GraphBackend.REF_GRAPH,
+    ],
+)
+def test_anyhashableg_strongly_gabow(backend):
     g = create_graph(
         directed=True,
         allowing_self_loops=False,
         allowing_multiple_edges=False,
         weighted=True,
-        any_hashable=True,
+        backend=backend,
+        edge_supplier=IntegerSupplier(),
+        vertex_supplier=IntegerSupplier(),
     )
 
     g.add_vertices_from(["0", "1", "2", "3", "4", "5"])
@@ -226,44 +296,3 @@ def test_anyhashableg_strongly_gabow():
 
     assert component1 == set(["0", "1", "2", "3", "4", "5"])
 
-
-def test_anyhashableg_strongly_kosaraju():
-    g = create_graph(
-        directed=True,
-        allowing_self_loops=False,
-        allowing_multiple_edges=False,
-        weighted=True,
-        any_hashable=True,
-    )
-
-    g.add_vertices_from([0, 1, 2, 3, 4, 5])
-    g.add_edge(0, 1)
-    g.add_edge(1, 2)
-    g.add_edge(2, 0)
-
-    g.add_edge(2, 3)
-
-    g.add_edge(3, 4)
-    g.add_edge(4, 5)
-    g.add_edge(5, 3)
-
-    is_connected, components = connectivity.is_strongly_connected_kosaraju(g)
-    assert not is_connected
-    component1 = next(components)
-    component2 = next(components)
-    with pytest.raises(StopIteration):
-        next(components)
-
-    assert component1 == set([0, 1, 2])
-    assert component2 == set([3, 4, 5])
-
-    g.add_edge(3, 2)
-
-    is_connected, components = connectivity.is_weakly_connected(g)
-    assert is_connected
-    component1 = next(components)
-    with pytest.raises(StopIteration):
-        next(components)
-
-    assert component1 == set([0, 1, 2, 3, 4, 5])
-    
