af9d3ca3dba04949604dbe4ffd820b489ad9ee0c
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-04-07 13:39:13 +0300

    Added map implementations

--- jgrapht/_internals/_collections_map.py
+++ jgrapht/_internals/_collections_map.py
@@ -11,7 +11,6 @@ from ._wrappers import (
     _JGraphTEdgeIntegerTripleIterator,
     _JGraphTEdgeStrTripleIterator,
     _JGraphTRefIterator,
-    _JGraphTRefIterator,
 )
 
 from collections.abc import (
@@ -516,3 +515,326 @@ class _JGraphTLongStringMap(_HandleWrapper, MutableMapping):
         items = ["{}: {}".format(k, v) for k, v in self.items()]
         return "{" + ", ".join(items) + "}"
 
+
+class _JGraphTRefIntegerMap(_HandleWrapper, Mapping):
+    """JGraphT Map"""
+
+    def __init__(self, handle, hash_equals_resolver_handle, **kwargs):
+        super().__init__(handle=handle, **kwargs)
+        self._hash_equals_resolver_handle = hash_equals_resolver_handle
+
+    def __iter__(self):
+        res = backend.jgrapht_xx_map_keys_it_create(self._handle)
+        return _JGraphTRefIterator(res)
+
+    def __len__(self):
+        res = backend.jgrapht_xx_map_size(self._handle)
+        return res
+
+    def get(self, key, value=None):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        if not res:
+            if value is not None:
+                return value
+            else:
+                raise KeyError()
+        res = backend.jgrapht_ri_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __contains__(self, key):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __getitem__(self, key):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        if not res:
+            raise KeyError()
+        res = backend.jgrapht_ri_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __repr__(self):
+        return "_JGraphTRefIntegerMap(%r)" % self._handle
+
+    def __str__(self):
+        items = ["{}: {}".format(k, v) for k, v in self.items()]
+        return "{" + ", ".join(items) + "}"
+
+
+class _JGraphTRefIntegerMutableMap(_JGraphTRefIntegerMap, MutableMapping):
+    """JGraphT Mutable Map"""
+
+    def __init__(self, handle, hash_equals_resolver_handle, **kwargs):
+        super().__init__(
+            handle=handle,
+            hash_equals_resolver_handle=hash_equals_resolver_handle,
+            **kwargs
+        )
+
+    def add(self, key, value):
+        key_exists = self.__contains__(key)
+        backend.jgrapht_ri_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    __marker = object()
+
+    def pop(self, key, defaultvalue=__marker):
+        try:
+            e = backend.jgrapht_ri_map_remove(
+                self._handle, id(key), self._hash_equals_resolver_handle
+            )
+            _ref_utils._dec_ref(key)
+            return e
+        except ValueError:
+            if defaultvalue is self.__marker:
+                raise KeyError()
+            else:
+                return defaultvalue
+
+    def __setitem__(self, key, value):
+        key_exists = self.__contains__(key)
+        backend.jgrapht_ri_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    def __delitem__(self, key):
+        key_exists = self.__contains__(key)
+        if not key_exists:
+            raise KeyError()
+        backend.jgrapht_ri_map_remove(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        _ref_utils._dec_ref(key)
+
+    def clear(self):
+        # Cleanup reference counts
+        for x in self:
+            _ref_utils._dec_ref(x)
+        backend.jgrapht_xx_map_clear(self._handle)
+
+    def __repr__(self):
+        return "_JGraphTRefIntegerMutableMap(%r)" % self._handle
+
+
+class _JGraphTRefDoubleMap(_HandleWrapper, Mapping):
+    """JGraphT Map"""
+
+    def __init__(self, handle, hash_equals_resolver_handle, **kwargs):
+        super().__init__(handle=handle, **kwargs)
+        self._hash_equals_resolver_handle = hash_equals_resolver_handle
+
+    def __iter__(self):
+        res = backend.jgrapht_xx_map_keys_it_create(self._handle)
+        return _JGraphTRefIterator(res)
+
+    def __len__(self):
+        res = backend.jgrapht_xx_map_size(self._handle)
+        return res
+
+    def get(self, key, value=None):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        if not res:
+            if value is not None:
+                return value
+            else:
+                raise KeyError()
+        res = backend.jgrapht_rd_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __contains__(self, key):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __getitem__(self, key):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        if not res:
+            raise KeyError()
+        res = backend.jgrapht_rd_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __repr__(self):
+        return "_JGraphTRefDoubleMap(%r)" % self._handle
+
+    def __str__(self):
+        items = ["{}: {}".format(k, v) for k, v in self.items()]
+        return "{" + ", ".join(items) + "}"
+
+
+class _JGraphTRefDoubleMutableMap(_JGraphTRefDoubleMap, MutableMapping):
+    """JGraphT Mutable Map"""
+
+    def __init__(self, handle, hash_equals_resolver_handle, **kwargs):
+        super().__init__(
+            handle=handle,
+            hash_equals_resolver_handle=hash_equals_resolver_handle,
+            **kwargs
+        )
+
+    def add(self, key, value):
+        key_exists = self.__contains__(key)
+        backend.jgrapht_rd_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    __marker = object()
+
+    def pop(self, key, defaultvalue=__marker):
+        try:
+            e = backend.jgrapht_rd_map_remove(
+                self._handle, id(key), self._hash_equals_resolver_handle
+            )
+            _ref_utils._dec_ref(key)
+            return e
+        except ValueError:
+            if defaultvalue is self.__marker:
+                raise KeyError()
+            else:
+                return defaultvalue
+
+    def __setitem__(self, key, value):
+        key_exists = self.__contains__(key)
+        backend.jgrapht_rd_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    def __delitem__(self, key):
+        key_exists = self.__contains__(key)
+        if not key_exists:
+            raise KeyError()
+        backend.jgrapht_rd_map_remove(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        _ref_utils._dec_ref(key)
+
+    def clear(self):
+        # Cleanup reference counts
+        for x in self:
+            _ref_utils._dec_ref(x)
+        backend.jgrapht_xx_map_clear(self._handle)
+
+    def __repr__(self):
+        return "_JGraphTRefDoubleMutableMap(%r)" % self._handle
+
+
+class _JGraphTRefStringMap(_HandleWrapper, MutableMapping):
+    """JGraphT Map with ref keys and string values"""
+
+    def __init__(self, handle, hash_equals_resolver_handle, **kwargs):
+        super().__init__(handle=handle, **kwargs)
+        self._hash_equals_resolver_handle = hash_equals_resolver_handle
+
+    def __iter__(self):
+        res = backend.jgrapht_xx_map_keys_it_create(self._handle)
+        return _JGraphTRefIterator(res)
+
+    def __len__(self):
+        res = backend.jgrapht_xx_map_size(self._handle)
+        return res
+
+    __marker = object()
+
+    def get(self, key, value=__marker):
+        key_exists = self.__contains__(key)
+        if not key_exists:
+            if value is self.__marker:
+                raise KeyError
+            else:
+                return value
+        res = backend.jgrapht_rs_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return str(_JGraphTString(res))
+
+    def add(self, key, value):
+        key_exists = self.__contains__(key)
+        encoded_value = bytearray(value, encoding="utf-8")
+        backend.jgrapht_rs_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, encoded_value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    def pop(self, key, defaultvalue=__marker):
+        try:
+            res = backend.jgrapht_ls_map_remove(self._handle, key)
+            _ref_utils._dec_ref(key)
+            return str(_JGraphTString(res))
+        except ValueError:
+            if defaultvalue is self.__marker:
+                raise KeyError()
+            else:
+                return defaultvalue
+
+    def __contains__(self, key):
+        res = backend.jgrapht_rx_map_contains_key(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return res
+
+    def __getitem__(self, key):
+        key_exists = self.__contains__(key)
+        if not key_exists:
+            raise KeyError()
+        res = backend.jgrapht_rs_map_get(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        return str(_JGraphTString(res))
+
+    def __setitem__(self, key, value):
+        key_exists = self.__contains__(key)
+        encoded_value = bytearray(value, encoding="utf-8")
+        backend.jgrapht_rs_map_put(
+            self._handle, id(key), self._hash_equals_resolver_handle, encoded_value
+        )
+        if not key_exists:
+            _ref_utils._inc_ref(key)
+
+    def __delitem__(self, key):
+        key_exists = self.__contains__(key)
+        if not key_exists:
+            raise KeyError()
+        backend.jgrapht_rs_map_remove(
+            self._handle, id(key), self._hash_equals_resolver_handle
+        )
+        _ref_utils._dec_ref(key)
+
+    def clear(self):
+        # Cleanup reference counts
+        for x in self:
+            _ref_utils._dec_ref(x)
+        backend.jgrapht_xx_map_clear(self._handle)
+
+    def __repr__(self):
+        return "_JGraphTRefStringMap(%r)" % self._handle
+
+    def __str__(self):
+        items = ["{}: {}".format(k, v) for k, v in self.items()]
+        return "{" + ", ".join(items) + "}"
--- tests/test_backend_maps.py
+++ tests/test_backend_maps.py
@@ -2,12 +2,18 @@ import pytest
 
 import jgrapht._backend as _backend
 
+from jgrapht._internals import _ref_hashequals
 from jgrapht._internals._collections_map import (
     _JGraphTIntegerStringMap,
     _JGraphTIntegerDoubleMap,
     _JGraphTIntegerDoubleMutableMap,
     _JGraphTIntegerIntegerMap,
     _JGraphTIntegerIntegerMutableMap,
+    _JGraphTRefIntegerMap,
+    _JGraphTRefIntegerMutableMap,    
+    _JGraphTRefDoubleMap,
+    _JGraphTRefDoubleMutableMap,
+    _JGraphTRefStringMap,
 )
 
 
@@ -220,4 +226,161 @@ def test_JGraphTIntegerDoubleMap():
     assert len(s) == 0
     repr(s)
 
-    
+
+
+def test_JGraphTRefDoubleMutableMap():
+
+    handle = _backend.jgrapht_xx_map_create()
+    hash_equals_resolver_handle = _ref_hashequals._get_hash_equals_wrapper().handle
+
+    s = _JGraphTRefDoubleMutableMap(
+        handle=handle, hash_equals_resolver_handle=hash_equals_resolver_handle
+    )
+
+    s[0] = 5.0
+    s[1] = 15.0
+
+    s.add(3, 150.0)
+
+    assert len(s) == 3
+
+    assert 0 in s
+    assert 1 in s
+    assert 2 not in s
+    assert 3 in s
+
+    assert s[0] == 5.0
+    assert s[1] == 15.0
+    assert s[3] == 150.0
+
+    keys = []
+    for k in s:
+        keys.append(k)
+    assert keys == [0, 1, 3]
+
+    assert s.get(0) == 5.0
+    assert s.get(2, 200.0) == 200.0
+
+    with pytest.raises(KeyError):
+        s.get(2)
+
+    with pytest.raises(KeyError):
+        print(s[2])
+
+    assert str(s) == "{0: 5.0, 1: 15.0, 3: 150.0}"
+
+    assert s.pop(3) == 150.0
+    assert s.pop(12, 17.0) == 17.0
+
+    with pytest.raises(KeyError):
+        assert s.pop(12)
+
+    del s[1]
+    assert 1 not in s
+
+    o = object()
+    s[o] = 199.0
+
+    assert o in s
+    assert s[o] == 199.0
+
+
+
+def test_JGraphTRefIntegerMutableMap():
+
+    handle = _backend.jgrapht_xx_map_create()
+    hash_equals_resolver_handle = _ref_hashequals._get_hash_equals_wrapper().handle
+
+    s = _JGraphTRefIntegerMutableMap(
+        handle=handle, hash_equals_resolver_handle=hash_equals_resolver_handle
+    )
+
+    s[0] = 5
+    s[1] = 15
+
+    s.add(3, 150)
+
+    assert len(s) == 3
+
+    assert 0 in s
+    assert 1 in s
+    assert 2 not in s
+    assert 3 in s
+
+    assert s[0] == 5
+    assert s[1] == 15
+    assert s[3] == 150
+
+    keys = []
+    for k in s:
+        keys.append(k)
+    assert keys == [0, 1, 3]
+
+    assert s.get(0) == 5
+    assert s.get(2, 200) == 200
+
+    with pytest.raises(KeyError):
+        s.get(2)
+
+    with pytest.raises(KeyError):
+        print(s[2])
+
+    assert str(s) == "{0: 5, 1: 15, 3: 150}"
+
+    assert s.pop(3) == 150
+    assert s.pop(12, 17) == 17
+
+    with pytest.raises(KeyError):
+        assert s.pop(12)
+
+    del s[1]
+    assert 1 not in s
+
+    with pytest.raises(KeyError):
+        del s[1]
+
+    repr(s)
+
+    s.clear()
+    assert len(s) == 0
+
+    o = object()
+    s[o] = 199
+
+    assert o in s
+    assert s[o] == 199
+
+    del s[o]
+    assert o not in s
+
+
+def test_JGraphTRefStringMutableMap():
+
+    handle = _backend.jgrapht_xx_map_linked_create()
+    hash_equals_resolver_handle = _ref_hashequals._get_hash_equals_wrapper().handle
+
+    s = _JGraphTRefStringMap(
+        handle=handle, hash_equals_resolver_handle=hash_equals_resolver_handle
+    )
+
+    o1 = object()
+    s[o1] = "myobject"
+    o2 = object()
+    s[o2] = "second"
+    o3 = object()
+
+    assert o1 in s
+    assert o2 in s
+    assert o3 not in s
+    assert s[o1] == "myobject"
+    assert s[o2] == "second"
+
+    assert len(s) == 2
+
+    with pytest.raises(KeyError):
+        del s[o3]
+
+    keys = []
+    for k in s:
+        keys.append(k)
+    assert keys == [o1, o2]
