b3ce38a4f1ec1faee7fd21ce67858d14590d2d63
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-04-10 08:45:26 +0300

    Added attributes tests

--- tests/test_backend_attributes.py
+++ tests/test_backend_attributes.py
@@ -1,19 +1,38 @@
 import pytest
 
+from jgrapht import create_graph, GraphBackend
+import jgrapht._backend as _backend
+
 from jgrapht._internals._attributes import (
-    _JGraphTIntegerAttributeStore,
+    _JGraphTAttributeStore,
     _JGraphTAttributesRegistry,
 )
 
 
-def test_store():
+def test_store_int_graph():
 
-    s = _JGraphTIntegerAttributeStore()
+    g = create_graph(backend=GraphBackend.INT_GRAPH)
+    handle = _backend.jgrapht_xx_attributes_store_create()
+    s = _JGraphTAttributeStore(handle=handle, graph=g)
 
     s.put(0, 'color', 'red')
     s.put(1, 'color', 'blue')
 
     s.remove(0, 'color')
+
+    repr(s)
+
+
+def test_store_ref_graph():
+
+    g = create_graph(backend=GraphBackend.REF_GRAPH)
+    handle = _backend.jgrapht_xx_attributes_store_create()
+    s = _JGraphTAttributeStore(handle=handle, graph=g)
+
+    s.put("0", 'color', 'red')
+    s.put("1", 'color', 'blue')
+
+    s.remove("0", 'color')
 
     repr(s)
 
