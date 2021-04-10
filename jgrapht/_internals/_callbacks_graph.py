39ca6ff37298f050f6c790203dc6ba23f5bd3b08
Dimitrios Michail <dimitrios.michail@gmail.com>
2021-04-10 10:05:12 +0300

    Added general callback wrappers for importers

--- jgrapht/_internals/_callbacks_graph.py
+++ jgrapht/_internals/_callbacks_graph.py
@@ -23,3 +23,85 @@ def _create_vertex_comparator_wrapper(graph, callback):
 
     return _callbacks._CallbackWrapper(callback, callback_ctype)
 
+
+def _create_import_id_wrapper(graph, callback, integer_input_ids=False):
+    if callback is None:
+        return _callbacks._CallbackWrapper(callback, callback_type=None)
+
+    if integer_input_ids:
+        if graph._backend_type == GraphBackend.INT_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+        elif graph._backend_type == GraphBackend.LONG_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(ctypes.c_longlong, ctypes.c_longlong)
+        elif graph._backend_type == GraphBackend.REF_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(
+                ctypes.ctypes.py_object, ctypes.c_longlong
+            )
+        else:
+            raise ValueError("Backend type invalid")
+        return _callbacks._CallbackWrapper(callback, callback_ctype)
+    else:
+        if graph._backend_type == GraphBackend.INT_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_char_p)
+        elif graph._backend_type == GraphBackend.LONG_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(ctypes.c_longlong, ctypes.c_char_p)
+        elif graph._backend_type == GraphBackend.REF_GRAPH:
+            callback_ctype = ctypes.CFUNCTYPE(ctypes.py_object, ctypes.c_char_p)
+        else:
+            raise ValueError("Backend type invalid")
+
+        # We wrap in order to decode string representation.
+        # There is no SWIG layer here, as the capi calls us directly
+        # using a function pointer. This means that the arguments
+        # are bytearrays.
+        def decoder_callback(id):
+            decoded_id = id.decode(encoding="utf-8")
+            return callback(decoded_id)
+
+        return _callbacks._CallbackWrapper(decoder_callback, callback_ctype)
+
+
+def _create_attribute_callback_wrapper(graph, callback):
+    if callback is None:
+        return _callbacks._CallbackWrapper(callback, callback_type=None)
+
+    if graph._backend_type == GraphBackend.INT_GRAPH:
+        id_type = ctypes.c_int
+    elif graph._backend_type == GraphBackend.LONG_GRAPH:
+        id_type = ctypes.c_longlong
+    elif graph._backend_type == GraphBackend.REF_GRAPH:
+        id_type = ctypes.py_object
+    else:
+        raise ValueError("Backend type invalid")
+
+    callback_ctype = ctypes.CFUNCTYPE(
+        None, id_type, ctypes.c_char_p, ctypes.c_char_p
+    )
+
+    # We wrap in order to decode string representation.
+    # There is no SWIG layer here, as the capi calls us directly
+    # using a function pointer. This means that the arguments
+    # are bytearrays.
+    def decoder_callback(id, key, value):
+        decoded_key = key.decode(encoding="utf-8")
+        decoded_value = value.decode(encoding="utf-8")
+        callback(id, decoded_key, decoded_value)
+
+    return _callbacks._CallbackWrapper(decoder_callback, callback_ctype)    
+
+
+def _create_notify_id_callback_wrapper(graph, callback):
+    if callback is None:
+        return _callbacks._CallbackWrapper(callback, callback_type=None)
+
+    if graph._backend_type == GraphBackend.INT_GRAPH:
+        id_type = ctypes.c_int
+    elif graph._backend_type == GraphBackend.LONG_GRAPH:
+        id_type = ctypes.c_longlong
+    elif graph._backend_type == GraphBackend.REF_GRAPH:
+        id_type = ctypes.py_object
+    else:
+        raise ValueError("Backend type invalid")
+
+    callback_ctype = ctypes.CFUNCTYPE(None, id_type)
+    return _callbacks._CallbackWrapper(callback, callback_ctype)    
--- jgrapht/_internals/_importers.py
+++ jgrapht/_internals/_importers.py
@@ -3,6 +3,12 @@ from collections import defaultdict
 from .. import backend as _backend
 from .. import GraphBackend as _GraphBackend
 
+from ._callbacks_graph import (
+    _create_import_id_wrapper,
+    _create_attribute_callback_wrapper,
+    _create_notify_id_callback_wrapper,
+)
+
 from ._ioutils import _create_wrapped_import_integer_id_callback
 from ._ioutils import _create_wrapped_import_string_id_callback
 from ._ioutils import _create_wrapped_attribute_callback
@@ -17,7 +23,40 @@ def _create_import_method(graph, read_from, format):
     else:
         graph_type = "ii"
     method_name = "jgrapht_{}_import_{}_{}".format(graph_type, read_from, format)
-    return getattr(_backend, method_name)        
+    return getattr(_backend, method_name)
+
+
+def _create_callback_wrappers(
+    graph,
+    import_id_cb,
+    vertex_attribute_cb,
+    edge_attribute_cb,
+    vertex_notify_id_cb,
+    edge_notify_id_cb,
+    integer_input_ids=False,
+):
+    import_id_wrapper = _create_import_id_wrapper(
+        graph, callback=import_id_cb, integer_input_ids=integer_input_ids
+    )
+    vertex_attribute_wrapper = _create_attribute_callback_wrapper(
+        graph, vertex_attribute_cb
+    )
+    edge_attribute_wrapper = _create_attribute_callback_wrapper(
+        graph, edge_attribute_cb
+    )
+    vertex_notify_id_wrapper = _create_notify_id_callback_wrapper(
+        graph, vertex_notify_id_cb
+    )
+    edge_notify_id_wrapper = _create_notify_id_callback_wrapper(
+        graph, edge_notify_id_cb
+    )
+    return (
+        import_id_wrapper,
+        vertex_attribute_wrapper,
+        edge_attribute_wrapper,
+        vertex_notify_id_wrapper,
+        edge_notify_id_wrapper,
+    )
 
 
 def _create_graph_callbacks(
@@ -64,7 +103,10 @@ def _create_graph_callbacks(
 
 
 def _parse_graph_dimacs(
-    graph, input, import_id_cb=None, input_is_filename=False,
+    graph,
+    input,
+    import_id_cb=None,
+    input_is_filename=False,
 ):
     (
         import_id_f_ptr,
@@ -157,37 +199,34 @@ def _parse_graph_json(
     input_is_filename=False,
 ):
     (
-        import_id_f_ptr,
-        import_id_f,  # pylint: disable=unused-variable
-        vertex_attribute_f_ptr,
-        vertex_attribute_f,  # pylint: disable=unused-variable
-        edge_attribute_f_ptr,
-        edge_attribute_f,  # pylint: disable=unused-variable
-        vertex_notify_f_ptr,
-        vertex_notify_f,  # pylint: disable=unused-variable
-        edge_notify_f_ptr,
-        edge_notify_f,  # pylint: disable=unused-variable
-    ) = _create_graph_callbacks(
+        import_id_wrapper,
+        vertex_attribute_wrapper,
+        edge_attribute_wrapper,
+        vertex_notify_id_wrapper,
+        edge_notify_id_wrapper,
+    ) = _create_callback_wrappers(
+        graph,
         import_id_cb=import_id_cb,
         vertex_attribute_cb=vertex_attribute_cb,
         edge_attribute_cb=edge_attribute_cb,
         vertex_notify_id_cb=None,
         edge_notify_id_cb=None,
+        integer_input_ids=False,
     )
-    
-    backend_method = _create_import_method(graph, "file" if input_is_filename else "string", "json")
+
+    backend_method = _create_import_method(
+        graph, "file" if input_is_filename else "string", "json"
+    )
 
     backend_method(
         graph.handle,
         bytearray(input, encoding="utf-8"),
-        import_id_f_ptr,
-        vertex_attribute_f_ptr,
-        edge_attribute_f_ptr,
-        vertex_notify_f_ptr,
-        edge_notify_f_ptr,
+        import_id_wrapper.fptr,
+        vertex_attribute_wrapper.fptr,
+        edge_attribute_wrapper.fptr,
+        vertex_notify_id_wrapper.fptr,
+        edge_notify_id_wrapper.fptr,
     )
-
-
 
 
 CSV_FORMATS = dict(
@@ -295,7 +334,6 @@ def _parse_graph_gexf(
     )
 
 
-
 def _parse_graph_dot(
     graph,
     input,
@@ -339,7 +377,6 @@ def _parse_graph_dot(
         vertex_notify_f_ptr,
         edge_notify_f_ptr,
     )
-
 
 
 def _parse_graph_graph6sparse6(
@@ -439,4 +476,3 @@ def _parse_graph_graphml(
         vertex_notify_f_ptr,
         edge_notify_f_ptr,
     )
-
